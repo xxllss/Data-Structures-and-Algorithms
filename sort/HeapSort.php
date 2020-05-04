048c924021e148475cbe0ff68aad98b1ab290bcc
xuliansheng <=xuliansheng@mafengwo.com>
2020-05-04 17:36:26 +0800

    添加php版本堆排序

--- sort/HeapSort.php
+++ sort/HeapSort.php
@@ -1,7 +1,37 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: xuliansheng
- * Date: 2020/5/4
- * Time: 下午5:16
- */
+
+function HeapAdjust(&$arr,$s,$length)
+{
+    $tmp = $arr[$s];
+    for ($i = $s * 2; $i < $length; $i *= 2) {
+        if ($arr[$i] < $arr[$i + 1]) {
+            ++$i;
+        }
+        if ($tmp > $arr[$i]) {
+            break;
+        }
+        $arr[$s] = $arr[$i];
+        
+        $s = $i;
+    }
+    $arr[$s] = $tmp;
+}
+
+function HeapSort(&$arr)
+{
+    $length = count($arr);
+    $step   = floor($length / 2);
+    
+    for ($i = $step; $i > 0; $i--) {
+        HeapAdjust($arr,$i,$length);
+    }
+    
+    for ($i = $length - 1; $i > 0; $i--) {
+        list($arr[0],$arr[$i]) = array($arr[$i],$arr[0]);
+        HeapAdjust($arr,0,$i - 1);
+    }
+}
+
+$arr = [9,5,8,3,7,4,6,2];
+HeapSort($arr);
+print_r($arr);
